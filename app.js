fe5cdfd450dfd59fea75c9fe79228220abcb97a9
Jim Disenstam <jim.disenstam@gmail.com>
2018-10-18 11:11:49 +0200

    Testing simple version of dealer vs player

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 'use strict'
 
-let hand = []
+let playerHand = []
+let dealerHand = []
+let playerHandSum = 0
+let dealerHandSum = 0
 
-function drawCard () {
-  let card = 9
-  return hand.push(card)
+function drawCard (who) {
+  let card = Math.floor(Math.random() * 14) + 1
+  return who.push(card)
 }
 
-function handSum () {
-  return hand.reduce((a, b) => a + b)
+function handSum (who) {
+  return who.reduce((a, b) => a + b)
 }
 
-function playerChoice () {
-  let playerHand
-  let threshold = 19
+function playerDraw () {
+  let threshold = 15
   do {
-    drawCard()
-    playerHand = handSum()
-  } while (playerHand < threshold)
-  if (playerHand > 21) {
-    return 'Player is Bust'
-  } else if (playerHand === 21) {
-    return 'Player Wins'
-  } else return playerHand
+    drawCard(playerHand)
+    playerHandSum = handSum(playerHand)
+  } while (playerHandSum < threshold)
+  return playerHandSum
+}
+
+function dealerDraw () {
+  if (playerHandSum < 21) {
+    do {
+      drawCard(dealerHand)
+      dealerHandSum = handSum(dealerHand)
+    } while (dealerHandSum < playerHandSum)
+  }
+  return dealerHandSum
+}
+
+function compareHands () {
+  if (playerHandSum > 21) {
+    return 'Player is Bust, Dealer Wins'
+  } else if (playerHandSum === 21) {
+    return '21! Player Wins'
+  } else if (dealerHandSum > 21) {
+    return 'Dealer is Bust, Player Wins'
+  } else return `Dealer Wins with ${dealerHandSum} vs player's ${playerHandSum}`
 }
 
 // drawCard()
@@ -30,6 +48,8 @@ function playerChoice () {
 // drawCard()
 // console.log(hand)
 // console.log(handSum())
-console.log(hand)
-console.log(playerChoice())
-console.log(hand)
+console.log(playerDraw())
+console.log(playerHand)
+console.log(dealerDraw())
+console.log(dealerHand)
+console.log(compareHands())
