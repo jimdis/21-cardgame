39cc4f359c61f81afce77a74d49dd6c76a0394c4
Jim Disenstam <jim.disenstam@gmail.com>
2018-10-19 13:29:04 +0200

    Adding 5 card rule, getting scoring of Aces working properly

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const deck = require('./src/deck')
 const Player = require('./src/Player')
 const Dealer = require('./src/Dealer')
 
-function game() {
+function game () {
   player1.drawCard()
   do {
     player1.drawCard()
-  } while (player1.score < player1.threshold)
+    player1.calculateScore()
+  } while (player1.score < player1.threshold && player1.hand.length < 5)
   if (player1.score > 21) {
     return 'Player is Bust, Dealer Wins!'
   } else if (player1.score === 21) {
     return 'Player Wins with 21!'
+  } else if (player1.hand.length === 5) {
+    return 'Player wins with 5 cards < 21'
   } else {
     do {
       dealer.drawCard()
+      dealer.calculateScore()
     } while (dealer.score < player1.score)
     if (dealer.score > 21) {
       return 'Dealer is Bust, Player Wins!'
@@ -29,9 +33,9 @@ function game() {
 let player1 = new Player(1)
 let dealer = new Dealer()
 deck.sort(() => Math.random() - 0.5) // test version!!
-console.log(deck)
-console.log(player1)
-console.log(dealer)
+// console.log(deck)
+// console.log(player1)
+// console.log(dealer)
 console.log(game())
-console.log(`Player's hand: ${player1.renderHand()}`)
-console.log(`Dealer's hand: ${dealer.renderHand()}`)
+console.log(`Player's hand: ${player1.renderHand()}, Score: ${player1.score}`)
+console.log(`Dealer's hand: ${dealer.renderHand()}, Score: ${dealer.score}`)
--- src/Dealer.js
+++ src/Dealer.js
@@ -7,24 +7,54 @@ class Dealer {
     this.hand = []
     this.score = 0
   }
+
   drawCard () {
     let card = deck.pop()
     this.hand.push(card)
-    if (typeof card.rank === 'number') {
-      this.score += card.rank
-    } else if (card.rank === 'J') {
-      this.score += 11
-    } else if (card.rank === 'Q') {
-      this.score += 12
-    } else if (card.rank === 'K') {
-      this.score += 13
-    } else if (card.rank === 'A') {
-      if (this.score < 8) {
-        this.score += 14
-      } else {
-        this.score += 1
+  }
+
+  calculateScore () {
+    let arr = []
+    for (let i = 0; i < this.hand.length; i++) {
+      let rank = this.hand[i].rank
+      if (typeof rank === 'number') {
+        arr.push(rank)
+      } else if (rank === 'J') {
+        arr.push(11)
+      } else if (rank === 'Q') {
+        arr.push(12)
+      } else if (rank === 'K') {
+        arr.push(13)
       }
     }
+    let score = arr.reduce((a, b) => a + b)
+    let aces = this.hand.filter(obj => obj.rank === 'A')
+    if (aces.length === 0) {
+      this.score = score
+    } else if (aces.length === 1) {
+      if (score < 8) {
+        score += 14
+      } else score += 1
+    } else if (aces.length === 2) {
+      if (score < 7) {
+        score += 15
+      } else {
+        score += 2
+      }
+    } else if (aces.length === 3) {
+      if (score < 6) {
+        score += 16
+      } else {
+        score += 3
+      }
+    } else if (aces.length === 4) {
+      if (score < 5) {
+        score += 17
+      } else {
+        score += 4
+      }
+    }
+    this.score = score
   }
 
   renderHand () {
